36888ad352dc30d5593d4f130803e18ca136b2c7
Reimia <reimia@qq.com>
2021-04-25 17:13:23 +0800

    更新插件版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     val kotlinVersion = "1.4.31"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
-    id("net.mamoe.mirai-console") version "2.5.1"
+    id("net.mamoe.mirai-console") version "2.6.2"
 }
 
 mirai{
